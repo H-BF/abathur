f7c30f5afba1f681e60a686208b259ca6ae4498f
kiver.nikita <kiver.nikita@wb.ru>
2023-09-29 16:47:48 +0300

    abathur proxy

--- control/sql/hbf.func.sql
+++ control/sql/hbf.func.sql
@@ -20,161 +20,63 @@
                     ('nw-6', '29.64.0.226/32', get_sg_id('sg-3')),
                     ('nw-7', '29.64.0.227/32', get_sg_id('sg-4')),
                     ('nw-8', '29.64.0.228/32', get_sg_id('sg-5')),
-                    ('nw-9', '29.64.0.229/32', get_sg_id('sg-5')),
-                    ('infra/report-server', 'HBF_REPORTER_IP/32', get_sg_id('infra/report-server')),
-                    ('infra/abathur-control', 'ABA_CONTROL_IP/32', get_sg_id('infra/abathur-control'));
+                    ('nw-9', '29.64.0.229/32', get_sg_id('sg-5'));
         
                 INSERT INTO
                     sgroups.tbl_sg_rule(sg_from, sg_to, proto, ports)
                 VALUES
                     (
-                    get_sg_id('sg-0'),
-                    get_sg_id('sg-1'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(NULL))), (int4multirange(int4range(50000, 50001))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                    get_sg_id('sg-1'),
-                    get_sg_id('sg-2'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(NULL))), (int4multirange(int4range(50001, 50004))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                    get_sg_id('sg-3'),
-                    get_sg_id('sg-5'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(NULL))), (int4multirange(int4range(50004,50005),int4range(50006,50007),int4range(50008,50011))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                    get_sg_id('sg-4'),
-                    get_sg_id('sg-0'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(41000, 41001))), (int4multirange(int4range(51001, 51002)))),
-                        ((int4multirange(int4range(41002, 41003))), (int4multirange(int4range(51003, 51006)))),
-                        ((int4multirange(int4range(41006, 41007))), (int4multirange(int4range(51007, 51008),int4range(51009,51010),int4range(51011,51014))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                    get_sg_id('sg-5'),
-                    get_sg_id('sg-2'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(42000, 42003))), (int4multirange(int4range(52003, 52004)))),
-                        ((int4multirange(int4range(42004, 42007))), (int4multirange(int4range(52007, 52010)))),
-                        ((int4multirange(int4range(42010, 42013))), (int4multirange(int4range(52013, 52014),int4range(52015,52016),int4range(52017,52020))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                    get_sg_id('sg-2'),
-                    get_sg_id('sg-1'),
-                    'tcp',
-                    ARRAY[
-                        ((int4multirange(int4range(43000,43001),int4range(43002,43003),int4range(43004,43007))), (int4multirange(int4range(53005,53006)))),
-                        ((int4multirange(int4range(43008,43009),int4range(43010,43011),int4range(43012,43015))), (int4multirange(int4range(53011,53014)))),
-                        ((int4multirange(int4range(43016, 43021))), (int4multirange(int4range(53019,53024))))
-                    ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
                         get_sg_id('sg-0'),
-                        get_sg_id('infra/report-server'),
+                        get_sg_id('sg-1'),
                         'tcp',
                         ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
+                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(50000, 50001))))
                         ]::sgroups.sg_rule_ports[]
                     ),
                     (
                         get_sg_id('sg-1'),
-                        get_sg_id('infra/report-server'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
                         get_sg_id('sg-2'),
-                        get_sg_id('infra/report-server'),
                         'tcp',
                         ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
+                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(50001, 50004))))
                         ]::sgroups.sg_rule_ports[]
                     ),
                     (
                         get_sg_id('sg-3'),
-                        get_sg_id('infra/report-server'),
+                        get_sg_id('sg-5'),
                         'tcp',
                         ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
+                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(50004,50005),int4range(50006,50007),int4range(50008,50011))))
                         ]::sgroups.sg_rule_ports[]
                     ),
                     (
                         get_sg_id('sg-4'),
-                        get_sg_id('infra/report-server'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                        get_sg_id('sg-5'),
-                        get_sg_id('infra/report-server'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(HBF_REPORTER_PORT_FROM, HBF_REPORTER_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
                         get_sg_id('sg-0'),
-                        get_sg_id('infra/abathur-control'),
                         'tcp',
                         ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                                (
-                        get_sg_id('sg-1'),
-                        get_sg_id('infra/abathur-control'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                        get_sg_id('sg-2'),
-                        get_sg_id('infra/abathur-control'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                        get_sg_id('sg-3'),
-                        get_sg_id('infra/abathur-control'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
-                        ]::sgroups.sg_rule_ports[]
-                    ),
-                    (
-                        get_sg_id('sg-4'),
-                        get_sg_id('infra/abathur-control'),
-                        'tcp',
-                        ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
+                            ((int4multirange(int4range(41000, 41001))), (int4multirange(int4range(51001, 51002)))),
+                            ((int4multirange(int4range(41002, 41003))), (int4multirange(int4range(51003, 51006)))),
+                            ((int4multirange(int4range(41006, 41007))), (int4multirange(int4range(51007, 51008),int4range(51009,51010),int4range(51011,51014))))
                         ]::sgroups.sg_rule_ports[]
                     ),
                     (
                         get_sg_id('sg-5'),
-                        get_sg_id('infra/abathur-control'),
+                        get_sg_id('sg-2'),
                         'tcp',
                         ARRAY[
-                            ((int4multirange(int4range(NULL))), (int4multirange(int4range(ABA_CONTROL_PORT_FROM, ABA_CONTROL_PORT_TO))))
+                            ((int4multirange(int4range(42000, 42003))), (int4multirange(int4range(52003, 52004)))),
+                            ((int4multirange(int4range(42004, 42007))), (int4multirange(int4range(52007, 52010)))),
+                            ((int4multirange(int4range(42010, 42013))), (int4multirange(int4range(52013, 52014),int4range(52015,52016),int4range(52017,52020))))
+                        ]::sgroups.sg_rule_ports[]
+                    ),
+                    (
+                        get_sg_id('sg-2'),
+                        get_sg_id('sg-1'),
+                        'tcp',
+                        ARRAY[
+                            ((int4multirange(int4range(43000,43001),int4range(43002,43003),int4range(43004,43007))), (int4multirange(int4range(53005,53006)))),
+                            ((int4multirange(int4range(43008,43009),int4range(43010,43011),int4range(43012,43015))), (int4multirange(int4range(53011,53014)))),
+                            ((int4multirange(int4range(43016, 43021))), (int4multirange(int4range(53019,53024))))
                         ]::sgroups.sg_rule_ports[]
                     );
                     INSERT INTO 
---
+++ control/src/domain/proxy/reporter.proxy.server.ts
@@ -0,0 +1,59 @@
+import http, { Server } from 'http';
+import httpProxy from 'http-proxy'
+import { variables } from '../../infrastructure/var_storage/variables-storage';
+
+class ReporterProxyServer {
+
+    private server: Server
+
+    constructor() {
+        const proxy = httpProxy.createProxyServer({});
+        this.server = http.createServer((req, res) => {
+
+            if (!req.headers['x-type'])
+                throw new Error("Отсутствует обязательный заголовок X-Type")
+
+            if (typeof req.headers['x-type'] !== 'string')
+                throw new Error("Не корректное значение X-Type. Должно быть string")
+
+            let target = this.selectTarget(req.headers['x-type'])
+            proxy.web(req, res, { target: target })
+        })
+    }
+
+    start() {
+        const proxyPort = variables.get("ABA_PROXY_PORT")
+        this.server.listen(proxyPort, () => {
+            console.log(`Proxy server start successfully on port ${proxyPort}`)
+        })
+    }
+
+    stop() {
+        this.server.close(() => {
+            console.log("Proxy server stopped")
+        })
+    }
+
+    private selectTarget(type: string): string {
+        let target: string
+        switch(type.toLowerCase()) {
+            case 'api':
+                const apiReportHost = variables.get("API_REPORTER_HOST")
+                const apiReportPort = variables.get("API_REPORTER_PORT")
+                const apiReportProtocol = variables.get("API_REPORTER_PROTOCOL")
+                target = `${apiReportProtocol}://${apiReportHost}:${apiReportPort}`
+                break;
+            case 'func':
+                const funcReportHost = variables.get("HBF_REPORTER_HOST")
+                const funcReportPort = variables.get("HBF_REPORTER_PORT")
+                const funcReportProtocol = variables.get("HBF_REPORTER_PROTOCOL")
+                target = `${funcReportProtocol}://${funcReportHost}:${funcReportPort}`
+                break;
+            default:
+                throw new Error(`не известный тип тестов: ${type}`)
+        }
+        return target
+    }
+}
+
+export const proxy = new ReporterProxyServer()
